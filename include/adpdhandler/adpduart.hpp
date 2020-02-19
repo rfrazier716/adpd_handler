bbd49890c6d8ae4a7d3a024b4fe2f6ba0801c0f5
Frazier, Ryan <Ryan.Frazier@analog.com>
2020-02-19 16:30:09 -0500

    Initial Commiit. This is a good project boilerplate for any mbed code with the nucleo F401RE. need the the static library which is hosted separately on google because of the large file size. Needs to be put in ./lib

---
+++ include/adpdhandler/adpduart.hpp
@@ -0,0 +1,10 @@
+#ifndef _ADPD_UART
+#define _ADPD_UART
+
+#include "../mbed-os/mbed.h"
+//TODO: This will hold the abstractions for reading and writing registers etc
+namespace adpd
+{
+    
+}
+#endif
---
+++ include/adpdhandler/logger.hpp
@@ -0,0 +1,10 @@
+#ifndef _ADPD_LOGGER
+#define _ADPD_LOGGER
+
+#include "mbed.h"
+#include ""
+namespace adpd
+{
+    
+}
+#endif
